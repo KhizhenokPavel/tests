813375fe5e81da35e1b91b6422c46d83a71a0ca9
Khizhenok Pavel <khizhenokp@gmail.com>
2023-11-09 15:42:14 +0300

    remove usless symols

--- php/check_mask.php
+++ php/check_mask.php
@@ -7,3 +7,12 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec($ch);
 $result = json_decode($result);
 curl_close($ch);
+
+foreach ($result as $phoneMask) {
+    $editPhoneNumber = str_replace(array(" ", "+", "-", "(", ")"), '', $phoneNumber);
+
+    $checkPhoneMask = $phoneMask->mask;
+    $checkPhoneMask = str_replace(array(" ", "+", "-", "(", ")"), '', $checkPhoneMask);
+}
+
+header("Location: /index.php?message=Введённый вами номер не принадлежит известным странам");
