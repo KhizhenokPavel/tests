52537835fe3e447fa5076e49204578fe164a1aae
Khizhenok Pavel <khizhenokp@gmail.com>
2023-11-09 15:28:32 +0300

    create popup html

--- html/banner.php
+++ html/banner.php
@@ -1,24 +1,21 @@
-<?php include "components/start.php"; ?>
-
-    <section class="banner-section position-relative">
-        <div class="big-ellipse"></div>
-        <div class="little-ellipse"></div>
-        <div class="container">
-            <div class="row">
-                <div class="image-container col-10 position-absolute"></div>
-                <div class="banner-info-container col-8">
-                    <h1 class="banner-container-title">Fulfillment dla Twojego <br>e-Commerce</h1>
-                    <ul class="banner-items-list">
-                        <li class="banner-list__item">Przyjęcie i magazynowanie produktów</li>
-                        <li class="banner-list__item">Kompletacja i pakowanie</li>
-                        <li class="banner-list__item">Obsługa zwrotów i reklamacji</li>
-                        <li class="banner-list__item">Współpraca ze wszystkimi dostępnymi przewoźnikami</li>
-                        <li class="banner-list__item">Integracja z Twoją platformą e-Commerce</li>
-                    </ul>
-                    <button class="banner-button">Więcej</button>
-                </div>
+<section class="banner-section position-relative">
+    <div class="big-ellipse"></div>
+    <div class="little-ellipse"></div>
+    <div class="container">
+        <div class="row">
+            <div class="image-container col-10 position-absolute"></div>
+            <div class="banner-info-container col-8">
+                <h1 class="banner-container-title">Fulfillment dla Twojego <br>e-Commerce</h1>
+                <ul class="banner-items-list">
+                    <li class="banner-list__item">Przyjęcie i magazynowanie produktów</li>
+                    <li class="banner-list__item">Kompletacja i pakowanie</li>
+                    <li class="banner-list__item">Obsługa zwrotów i reklamacji</li>
+                    <li class="banner-list__item">Współpraca ze wszystkimi dostępnymi przewoźnikami</li>
+                    <li class="banner-list__item">Integracja z Twoją platformą e-Commerce</li>
+                </ul>
+                <button class="banner-button">Więcej</button>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-<?php include "components/end.php"; ?>
--- index.php
+++ index.php
@@ -1,1 +1,5 @@
+<?php include "html/components/start.php"; ?>
+
 <?php include "html/banner.php"; ?>
+
+<?php include "html/components/end.php"; ?>
