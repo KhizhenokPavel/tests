11b1109f4c3bc341629a6ece4f9461af77d661fc
Khizhenok Pavel <khizhenokp@gmail.com>
2023-11-09 14:24:33 +0300

    insert text in banner structure

--- html/banner.php
+++ html/banner.php
@@ -7,15 +7,15 @@
             <div class="row">
                 <div class="image-container"></div>
                 <div class="banner-info-container">
-                    <h1 class="banner-container-title"></h1>
+                    <h1 class="banner-container-title">Fulfillment dla Twojego e-Commerce</h1>
                     <ul class="banner-items-list">
-                        <li class="banner-list__item"></li>
-                        <li class="banner-list__item"></li>
-                        <li class="banner-list__item"></li>
-                        <li class="banner-list__item"></li>
-                        <li class="banner-list__item"></li>
+                        <li class="banner-list__item">Przyjęcie i magazynowanie produktów</li>
+                        <li class="banner-list__item">Kompletacja i pakowanie</li>
+                        <li class="banner-list__item">Obsługa zwrotów i reklamacji</li>
+                        <li class="banner-list__item">Współpraca ze wszystkimi dostępnymi przewoźnikami</li>
+                        <li class="banner-list__item">Integracja z Twoją platformą e-Commerce</li>
                     </ul>
-                    <button class="banner-button"></button>
+                    <button class="banner-button">Więcej</button>
                 </div>
             </div>
         </div>
